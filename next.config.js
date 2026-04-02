bdff5aea8c1ea480c818cfc400f65698008df2e9
TheBuzzCRM <daron@thebuzzcrm.com>
2026-04-02 11:37:20 -0400

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['buyerintentleads.com', 'localhost:3000'],
+    },
+  },
+}
+
+module.exports = nextConfig
